8a60e3a9529fbb8250b0ec1b99484f3b2e48d862
Wanderinguserxxxxxxx <sahilkumar3570218@gmail.com>
2026-02-07 18:58:58 +0530

    Implement addProduct using ArrayList

--- main/src/InventoryService.java
+++ main/src/InventoryService.java
@@ -1,6 +1,20 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class InventoryService {
-    public void addProduct(){}
-    public void viewProduct(){}
-    public void updateProduct(){}
-    public void removeProduct(){}
+    private List<Product> products = new ArrayList<>();
+
+    public void addProduct(int id, String name, int quantity, double price) {
+        Product product=new Product(id,name,quantity,price);
+        products.add(product);
+    }
+
+    public void viewProducts() {
+    }
+
+    public void updateProduct() {
+    }
+
+    public void removeProduct() {
+    }
 }
